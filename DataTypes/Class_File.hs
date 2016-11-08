99baf98fdc759ac1c5d0ea34e1264c28c5fcce38
Louis Jenkins <theif517@gmail.com>
2016-11-07 23:12:19 -0500

    Added some minor documentation for the presentation. I need to pick out some code snippets, and so this would probably be one of them, as it contains so much packed into one.

--- DataTypes/Class_File.hs
+++ DataTypes/Class_File.hs
@@ -24,12 +24,49 @@ module DataTypes.Class_File where
       classfile_attributes :: [Attribute_Info]
   } deriving (Show)
 
+  {-
+    Parameter arguments are specified as such: (x -> y -> z), wherein it will take 'x' and 'y'
+    and return 'z'. Some arguments however are implicit, such as monads, which implicitly
+    take and return a state.
+
+    'parseClassFile' takes a 'StateT' monad, which implicitly passes the state of a
+    'ByteString' with each invocation. By doing so it allows it to simulate side-effects
+    while remaining functionally pure, as the results of the previous computation carries
+    on to the next without causing an actual side-effect.
+
+    The 'StateT' monad can use 'evalState' to immediately evaluate all computations and return
+    the result (Remember, Haskell is very lazy, and computations are not evaluated until needed,
+    this would force such evaluations).
+
+    Monads generally use binding, either with the '>>=' infix bind operator, which
+    obtains the result of the left-hand side computation and passes it to the right-hand side.
+    The '>>' operator will discard the result but complete the computation, which is handy for
+    discarding irrelevant data.
+
+    The <$> operator is the 'fmap' infix operator, which applies the functor ('StateT' is a monadic functor)
+    to the result of applying a mapping of an input to an output. I.E: (+1) <$> Just 2 => Just 3
+
+    The <*> operator is the 'sequence' inifx operator, which is similar to bind, except it is left-associative.
+    It works with the below due to currying: Currying is the process of taking a function, say (x -> y -> z),
+    supplying a partial application, resulting in (y -> z) where 'x' has been given a value. Hence the next sequential
+    operation can supply the remaining argument 'y'. Example...
+
+    (x -> y -> z) <$> getX => (y -> z),
+    (x -> y -> z) <$> getX <*> getY => z.
+
+    The 'replicateM' performs the passed action 'n' times and combines the result into a list.
+
+    Syntax such as: (\x -> x + 1) constitute a lambda function.
+  -}
   parseClassFile :: ByteString -> ClassFile
   parseClassFile = evalState $ getNextInt >> getNextShort >> getNextShort -- Discard magic and version information
     >> parseConstants >>= \cp -> ClassFile cp <$> getNextShort <*> getNextShort <*> getNextShort
     <*> parseInterfaces <*> parseFields cp <*> parseMethods cp <*> parseAttributes cp
       where
         parseInterfaces = getNextShort >>= \n -> replicateM (fromIntegral n) getNextShort
+
+
+
 
   main :: IO ()
   main = do
