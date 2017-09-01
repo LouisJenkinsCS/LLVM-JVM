c2cba295a456a453daa69e1a6692521ffe63e5a3
Louis Jenkins <LouisJenkinsCS@hotmail.com>
2017-09-01 18:26:19 -0400

    Unit test now searches all sub directories

--- Test.bash
+++ Test.bash
@@ -10,24 +10,32 @@ else
 fi
 
 # All files should be in the 'Tests' directory
-cd Unit/Tests
+cd Unit
 
-# We only search for files with the 'java' extension
-for f in *.java; do
-  filename=$(basename "${f%.*}")
-  javac $f
-  ../../Main.exe "$filename.class" 1> tmp.out 2> tmp.err
-  if [ $? -eq 0 ]; then
-    echo "[$filename]: SUCCESS"
-  else
-    echo "[$filename]: FAILURE"
-    cat tmp.err
-  fi
+# Search all directories
+for d in */; do
+  cd $d
+  echo "cd $d"
 
-  rm "$filename.class"
-  rm tmp.out
-  rm tmp.err
+  # We only search for files with the 'java' extension
+  for f in *.java; do
+    filename=$(basename "${f%.*}")
+    javac $f
+    ../../Main.exe "$filename.class" 1> tmp.out 2> tmp.err
+    if [ $? -eq 0 ]; then
+      echo "[$filename]: SUCCESS"
+    else
+      echo "[$filename]: FAILURE"
+      cat tmp.err
+    fi
 
+    rm "$filename.class"
+    rm tmp.out
+    rm tmp.err
+
+  done
+
+  cd ../
 done
 
-cd ../..
+cd ../
