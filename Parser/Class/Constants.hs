6309b520902c2857c9b47f78fcf51f87bf450453
Louis Jenkins <LouisJenkinsCS@hotmail.com>
2017-09-02 15:33:58 -0400

    Implemented ParseConstant with Parsec

--- Parser/Class/Constants.hs
+++ Parser/Class/Constants.hs
@@ -8,65 +8,27 @@ module Parser.Class.Constants where
 
   -- Imports for helper methods
   import Parser.Class.Helpers (getWord8, getWord16, getWord32, getWord64)
-
-
+  import Control.Monad (replicateM)
 
   {-
     Types for Constant Pool
   -}
 
-  data CPConstant =
-    -- Class
-    CPClass {
-      nameIndex :: Word16
-    } |
-    CPFieldref {
-      classIndex :: Word16,
-      nameAndTypeIndex :: Word16
-    } |
-    CPMethodref {
-      classIndex :: Word16,
-      nameAndTypeIndex :: Word16
-    } |
-    CPInterfaceMethodref {
-      classIndex :: Word16,
-      nameAndTypeIndex :: Word16
-    } |
-    CPString {
-      stringIndex :: Word16
-    } |
-    CPInteger {
-      bytes :: Word32
-    } |
-    CPFloat {
-      bytes :: Word32
-    } |
-    CPLong {
-      highBytes :: Word32,
-      lowBytes :: Word32
-    } |
-    CPDouble {
-      highBytes :: Word32,
-      lowBytes :: Word32
-    } |
-    CPNameAndType {
-      nameIndex :: Word16,
-      descriptorIndex :: Word16
-    } |
-    CPUtf8 {
-      utf8Bytes :: [Word16]
-    } |
-    CPMethodHandle {
-      referenceKind :: Word8,
-      referenceIndex :: Word16
-    } |
-    CPMethodType {
-      descriptorIndex :: Word16
-    } |
-    CPInvokeDynamic {
-      bootstrapMethodAttrIndex :: Word16,
-      nameAndTypeIndex :: Word16
-    }
+  -- Constant Pool types
+  data CPConstant = CPClass { nameIndex :: Word16 }
+    | CPFieldref { classIndex :: Word16, nameAndTypeIndex :: Word16 }
+    | CPMethodref { classIndex :: Word16, nameAndTypeIndex :: Word16 }
+    | CPInterfaceMethodref { classIndex :: Word16, nameAndTypeIndex :: Word16 }
+    | CPString { stringIndex :: Word16 }
+    | CPInteger { bytes :: Word32 }
+    | CPFloat { bytes :: Word32 }
+    | CPLong { highBytes :: Word32, lowBytes :: Word32 }
+    | CPDouble { highBytes :: Word32, lowBytes :: Word32 }
+    | CPNameAndType { nameIndex :: Word16, descriptorIndex :: Word16 }
+    | CPUtf8 { utf8Bytes :: [Word8] }
+    | CPMethodHandle { referenceKind :: Word8, referenceIndex :: Word16 }
+    | CPMethodType { descriptorIndex :: Word16 }
+    | CPInvokeDynamic { bootstrapMethodAttrIndex :: Word16, nameAndTypeIndex :: Word16 }
 
   -- Custom 'showable' for better debugging
   instance Show CPConstant where
@@ -148,11 +110,35 @@ module Parser.Class.Constants where
         ",name_and_type_index=" ++ show _nameAndTypeIndex ++
       "}"
 
+  {-
+    Parse functions
+  -}
 
-  -- The Constant Pool consists of a 16-bit count (denoting the amount of constants
-  -- this class file holds) followed by `cp_info` structures, the constants themselves.
-  -- Note: All indexes are 0-based.
-  -- parseConstants :: Parser [CPConstant]
-  -- parseConstants = do
-  --   cpCount <- getWord16
-  --   return
+  -- Parses all constant pool objects. The first word must be the classfile's
+  -- 'constant_pool_count', which specifies the number of constants that follow.
+  parseConstants :: Parser [CPConstant]
+  parseConstants = do
+    cpCount <- getWord16
+    replicateM (fromIntegral cpCount - 1) parseConstant
+
+  -- Parse a constant object by its tag. The first word must be a valid tag for
+  -- a constant pool object.
+  parseConstant :: Parser CPConstant
+  parseConstant = do
+    tag <- getWord8
+    case tag of
+      7 -> CPClass <$> getWord16
+      9 -> CPFieldref <$> getWord16 <*> getWord16
+      10 -> CPMethodref <$> getWord16 <*> getWord16
+      11 -> CPInterfaceMethodref <$> getWord16 <*> getWord16
+      8 -> CPString <$> getWord16
+      3 -> CPInteger <$> getWord32
+      4 -> CPFloat <$> getWord32
+      5 -> CPLong <$> getWord32 <*> getWord32
+      6 -> CPDouble <$> getWord32 <*> getWord32
+      12 -> CPNameAndType <$> getWord16 <*> getWord16
+      1 -> CPUtf8 <$> (getWord16 >>= \len -> replicateM (fromIntegral len) getWord8)
+      15 -> CPMethodHandle <$> getWord8 <*> getWord16
+      16 -> CPMethodType <$> getWord16
+      18 -> CPInvokeDynamic <$> getWord16 <*> getWord16
+      _ -> error ("Bad Constant Tag: " ++ show tag)
