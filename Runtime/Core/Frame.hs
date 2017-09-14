388bb9ff3b5b0264896944783c4784324e85a91e
Louis Jenkins <LouisJenkinsCS@hotmail.com>
2017-09-14 11:42:28 -0400

    Began a major refactor of the runtime. Going to be phasing out the old multiple modules into one bigger one. I believe this will be the best approach in the longrun and it seems to have gotten miraculously easier to write code

--- Runtime/Core/Frame.hs
+++ Runtime/Core/Frame.hs
@@ -4,31 +4,4 @@ module Runtime.Core.Frame where
   import Data.Array.IO
   import Control.Monad.IO.Class
 
-  load :: Integral a => a -> Runtime Variable
-  load idx = do
-    frame <- currentFrame
-    liftIO $ readArray (variables frame) (fromIntegral idx)
-
-  store :: Integral a => a -> Variable -> Runtime ()
-  store idx var = do
-    frame <- currentFrame
-    liftIO $ writeArray (variables frame) (fromIntegral idx) var
-
-  modify :: Integral a => a -> (Variable -> Variable) -> Runtime ()
-  modify idx f = do
-    var <- load idx
-    store idx (f var)
-
-  push :: Variable -> Runtime ()
-  push var = do
-    frame <- currentFrame
-    liftIO $ do
-      idx <- atomicModifyIORef' (operandTop frame) (\x -> (x + 1, x))
-      writeArray (operands frame) idx var
-
-  pop :: Runtime Variable
-  pop = do
-    frame <- currentFrame
-    liftIO $ do
-      idx <- atomicModifyIORef' (operandTop frame) (\x -> (x - 1, x - 1))
-      readArray (operands frame) idx
+  
